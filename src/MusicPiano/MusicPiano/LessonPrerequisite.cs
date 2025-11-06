519bdbd95515ae642076ed4437c78eab82cdbee9
Ionut Niculici <ionut_SC94@yahoo.com>
2025-11-06 16:09:38 +0200

    Used a semaphore to ensure only one update runs at a time

---
+++ src/MusicPiano/MusicPiano/LessonPrerequisite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MusicPiano;
+
+public partial class LessonPrerequisite
+{
+    public int IdLesson { get; set; }
+
+    public int PrerequisiteLessonId { get; set; }
+
+    public virtual Lesson IdLessonNavigation { get; set; } = null!;
+}
--- src/MusicPiano/MusicPiano/Program.cs
+++ src/MusicPiano/MusicPiano/Program.cs
@@ -1,5 +1,92 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MusicPiano;
 using MusicPianoLogic;
+
+#region Database Setup
+using var context = new PianoLessonContext();
+SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
+// Create
+/*await _semaphore.WaitAsync();
+try
+{
+    var customers = new List<MusicPiano.Lesson>
+    {
+        new MusicPiano.Lesson {
+            Name = "Reading",
+            Description = "How to read sheet music"
+        },
+        new MusicPiano.Lesson {
+            Name = "Counting",
+            Description = "How to count basic rhythms"
+        },
+        new MusicPiano.Lesson {
+            Name = "Chords",
+            Description = "How to add chords to a melody"
+        },
+        new MusicPiano.Lesson {
+            Name = "Symbols",
+            Description = "Learn the basic musical symbols"
+        },
+        new MusicPiano.Lesson {
+            Name = "Arpeggios",
+            Description = "How to play arpeggios on the piano"
+        }
+    };
+
+    context.Lessons.AddRange(customers);
+    await context.SaveChangesAsync();
+}
+finally
+{
+    _semaphore.Release();
+}*/
+
+// Read
+/*await _semaphore.WaitAsync();
+try
+{
+    context.Lessons.ToList().ForEach(lesson =>
+    {
+        Console.WriteLine($"Lesson ID: {lesson.Id}, Name: {lesson.Name}, Description: {lesson.Description}");
+    });
+}
+finally
+{
+    _semaphore.Release();
+}*/
+
+// Update
+/*await _semaphore.WaitAsync();
+try
+{
+    context.Lessons.Where(lesson => lesson.Name == "Reading").ToList().ForEach(lesson =>
+    {
+        lesson.Description = "Learn to read sheet music effectively";
+    });
+    await context.SaveChangesAsync();
+}
+finally
+{
+    _semaphore.Release();
+}*/
+
+// Delete
+/*await _semaphore.WaitAsync();
+try
+{
+    context.Lessons.Where(lesson => lesson.Name == "Arpeggios").ToList().ForEach(lesson =>
+    {
+        context.Lessons.Remove(lesson);
+    });
+    await context.SaveChangesAsync();
+}
+finally
+{
+    _semaphore.Release();
+}*/
+
+#endregion
 
 Console.WriteLine("Hello to the music piano!");
 
