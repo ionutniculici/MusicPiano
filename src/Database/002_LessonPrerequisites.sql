f3cfbfb15e8a19c508cb51ead07dc2c2dca6be29
Ionut Niculici <ionut_SC94@yahoo.com>
2025-11-07 08:52:15 +0200

    Adding scripts for database

---
+++ src/Database/002_LessonPrerequisites.sql
@@ -0,0 +1,26 @@
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.LessonPrerequisites
+	DROP CONSTRAINT PK_LessonPrerequisites
+GO
+ALTER TABLE dbo.LessonPrerequisites ADD CONSTRAINT
+	PK_LessonPrerequisites_1 PRIMARY KEY CLUSTERED 
+	(
+	IdLesson,
+	PrerequisiteLessonId
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.LessonPrerequisites SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
