f609bcf75694156ff40be67e77b620a4ffaf063d
Ionut Niculici <ionut_SC94@yahoo.com>
2025-11-28 10:14:32 +0200

    Adaugat proiect web

---
+++ src/MusicPiano/MusicPianoWeb/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicPianoBusinessLogic;
+using MusicPianoData;
+using MusicPianoWeb.Classes;
+
+namespace MusicPianoWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        [HttpGet("hello")]
+        public async Task<string> GetHelloAsync()
+        {
+            await Task.Delay(100); // Simulate some asynchronous work
+            return "Hello user";
+        }
+
+        [HttpPost()]
+        public async Task<int> LoginUser(UserLogin login)
+        {
+            using PianoLessonContext context = new PianoLessonContext();
+            Repository repository = new Repository(context);
+            User? user = await repository.LoginUser(login.username, login.password);
+            return user?.Id ?? -1;
+        }
+    }
+}
---
+++ src/MusicPiano/MusicPianoWeb/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicPianoWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("hello")]
+        public async Task<string> GetHelloAsync()
+        {
+            await Task.Delay(100); // Simulate some asynchronous work
+            return "Hello, Music Piano!";
+        }
+    }
+}
